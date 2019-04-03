d25d9a0fa65c49f945f526f6d15162261fc0992a
Sirius.Black <ofghrt@qq.com>
2019-04-03 17:17:04 +0800

    update

--- 1002.js
+++ 1002.js
@@ -6,5 +6,15 @@ const rl = readline.createInterface({
 
 
 rl.on('line', (cmd) => {
-	rl.close() ;
+	const arr = cmd.split(" ") ;
+	arr[0] = arr[0] / 1  ;
+	arr[1] = arr[1] / 1 ;
+	if( arr[0] > 0 ){
+		console.log(arr[0].toString(2).split("").reverse().join("")) ;
+		console.log(arr[0].toString(2) )
+		const isEq = arr[0].toString(2).split("").reverse().join("") === arr[0].toString(2) ;
+		const isEq2 =  arr[1].toString(2).split("").reverse().join("") === arr[1].toString(2) ;
+		console.log(isEq && isEq2 ? "YES" :"NO") ;
+	}
+	// rl.close() ;
 });
